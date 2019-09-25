031bef6fb9cf35bd569db90ff058b23c57a7129b
chenziwei1 <rickycs97@pku.edu.cn>
2019-09-25 12:49:05 +0800

    fking typo.

--- MIXUP/mixup_utils.py
+++ MIXUP/mixup_utils.py
@@ -7,7 +7,7 @@ def mixup_data(x, y, alpha=1.0, use_cuda=True):
         lam = np.random.beta(alpha,alpha)
     else:
         lam = 1
-    batch_size = x_size()[0]
+    batch_size = x.size()[0]
     if use_cuda:
         index = torch.randperm(batch_size).cuda()
     else:
